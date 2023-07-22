039568c3a067da0510b907daca3fe57b4b1722aa
Wym <dewreator2@gmail.com>
2023-07-22 21:12:02 +0200

    Env secret

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,12 @@
 import unittest
+import os
 from src.main import get_name
 
 class TestMain(unittest.TestCase):
     def test_name(self):
         name = "John"
         expected = "Hello John"
+        print(os.getenv("MODE"))
         self.assertEqual(get_name(name), expected)
 
 if __name__ == "__main__":
