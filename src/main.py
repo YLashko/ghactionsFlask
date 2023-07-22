6964f92fe830f6db01c9291e61bac826d6d1967d
Wym <dewreator2@gmail.com>
2023-07-22 14:38:38 +0200

    Base

---
+++ src/main.py
@@ -0,0 +1,17 @@
+from flask import Flask, Response, render_template
+import os
+from random import randint
+
+app = Flask(__name__)
+
+@app.route("/<name>", methods=["GET"])
+def get_name(name):
+    return render_template("name.html.jinja", name=name)
+
+@app.route("/", methods=["GET"])
+def main():
+    number = " ".join([str(randint(0, 9)) for _ in range(200)])
+    return render_template("main.html.jinja", number=number, mode=os.environ.get("MODE"))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
